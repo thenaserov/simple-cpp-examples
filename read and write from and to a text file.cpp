a37e8644ea5a3469500aa57a37977752912e4455
naserrezayi1998 <naser.rezayi.nr@gmail.com>
2023-03-09 18:54:04 +0330

    [ADD] this is just for a single text reading

---
+++ read and write from and to a text file.cpp
@@ -0,0 +1,32 @@
+// *************************************************************************
+// ****************************** 2023.03.09 *******************************
+// ****************** simple C++ examples By Naser Rezayi ******************
+// ***************** READ AND WRITE FROM AND TO A TXT FILE *****************
+// *************************************************************************
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+	std::fstream file;
+
+	// WRITE OPERTATION
+	file.open("file.txt", std::ios::out);
+	if (file.is_open())
+	{
+		file << "my keyboard is great\n";
+		file.close();
+	}
+
+	// READ OPERATION
+	file.open("file.txt", std::ios::in);
+    if (file.is_open()) {
+        std::string text;
+        while (std::getline(file, text)) {
+            std::cout << text << "\n";
+        }
+        file.close();
+    }
+	return 0;
+}
