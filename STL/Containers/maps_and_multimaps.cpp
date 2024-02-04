80edbb1b619509db913147f7419d389211def12a
thenaserov <naser.rezayi.nr@gmail.com>
2024-02-04 15:04:00 +0330

    [ADD] more exp on maps

--- STL/Containers/maps_and_multimaps.cpp
+++ STL/Containers/maps_and_multimaps.cpp
@@ -7,7 +7,7 @@ int main() {
 	// This Key -> Value association allows you to access elements by "name"
 	// instead of simply by index or position
 
-	// Accessor methods for these containers
+	// Accessor m
     // ------------------------------------------------------------------------------------------------------------------------
     // |     Contaianer      |   Associative  |   Oerdered  |    Mapped    |    Unique Keys    |    Hashing    |   Dynamic    | 
     // |        map          |        x       |      x      |       x      |         x         |               |       x      | 
@@ -16,6 +16,20 @@ int main() {
     // | unnordered_multimap |        x       |             |       x      |         x         |       x       |       x      |
     // |----------------------------------------------------------------------------------------------------------------------|
 
+	// *** MAP and UNORDERED_MAP only accept UNIQUE keys
+	
+	// Map containers are generally slower thann unordered_map containers to access 
+	// individual elements by their key, nut they allow direct iteration on subsets based on their order.
+	
+	//Maps only allow unique keys, and will overwrite or reject insertions with the same key depending
+	// on how the operation was performed.
+
+	// *** In multimaps you can have multiple equivalent keys.
+	// Multimaps also do not implement the operator []() functionality, you must use multimap::find()
+	// to find the key you want!
+	// find : will return the first element found by key.
+	
+	
 
 	return 0;
 }
