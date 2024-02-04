d8b18792b8ea748fa1684ee0809aac5ff9b8a08e
thenaserov <naser.rezayi.nr@gmail.com>
2024-02-04 12:36:00 +0330

    [ADD] maps and multimaps (exp) only

---
+++ STL/Containers/maps_and_multimaps.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+
+int main() {
+	// A map is an associative container that stores pairwise elements
+	// Each element has a key and value
+	// This Key -> Value association allows you to access elements by "name"
+	// instead of simply by index or position
+
+	// Accessor methods for these containers
+    // ------------------------------------------------------------------------------------------------------------------------
+    // |     Contaianer      |   Associative  |   Oerdered  |    Mapped    |    Unique Keys    |    Hashing    |   Dynamic    | 
+    // |        map          |        x       |      x      |       x      |         x         |               |       x      | 
+    // |      multimap       |        x       |      x      |       x      |                   |               |       x      |
+    // |    unordered_map    |        x       |             |       x      |                   |       x       |       x      |
+    // | unnordered_multimap |        x       |             |       x      |         x         |       x       |       x      |
+    // |----------------------------------------------------------------------------------------------------------------------|
+
+
+	return 0;
+}
