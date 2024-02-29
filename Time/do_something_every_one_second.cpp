12a2f8d192bd98f93447e03b8e84159cac2ddd12
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2024-02-29 07:59:36 +0330

    Create do_something_every_one_second.cpp

---
+++ Time/do_something_every_one_second.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <chrono>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+// TheNaserov
+int main() {
+	auto start = std::chrono::steady_clock::now();
+	while (true) {
+		auto end = std::chrono::steady_clock::now();
+		auto diff = end - start;
+		if (std::chrono::duration_cast<std::chrono::seconds>(diff).count() >= 1) {
+			std::cout << "doing sth" << std::endl;
+			start = std::chrono::steady_clock::now();
+		}
+	}
+	return 0;
+}
+
