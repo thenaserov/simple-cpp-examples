11bcffa081acbc7e92fe95e8bfc31b0c02752985
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2024-03-19 22:34:54 +0330

    Update Maps.cpp

--- STL/Containers/Maps.cpp
+++ STL/Containers/Maps.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <map>
 
+struct SimpleObject{
+    int x;
+    int y;
+    std::string z;
+
+    SimpleObject() : x(1), y(2), z("Something"){
+
+    }
+    SimpleObject(int x, int y, std::string z): x(x), y(y), z(z){
+
+    }
+};
+
+
+int main()
+{
+    std::cout << "Maps" << std::endl;
+    std::map<std::string, std::string> stringMap;
+    stringMap.insert({"Hello", "World"});
+    std::cout << stringMap["Hello"] << std::endl;
+    
+    return 0;
+}
