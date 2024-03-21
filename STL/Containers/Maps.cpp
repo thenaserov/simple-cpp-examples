bde7bfc28d02b3cf47e1e7d9dd0e1ddb75a191ff
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2024-03-21 09:56:29 +0330

    Update Maps.cpp

--- STL/Containers/Maps.cpp
+++ STL/Containers/Maps.cpp
@@ -21,6 +21,20 @@ int main()
     std::map<std::string, std::string> stringMap;
     stringMap.insert({"Hello", "World"});
     std::cout << stringMap["Hello"] << std::endl;
+
+    std::map<std::string, SimpleObject> mMap;
+    // insert in pair constructor
+    mMap.insert(std::pair<std::string, SimpleObject>("first", SimpleObject(4, 8, "Hello")));
+    // bracket notation init
+    mMap["second"] = SimpleObject();
+    // overwrite the "second"
+    mMap["second"] = SimpleObject(0, 0, "overwritten");
+    // auto 
+    mMap["third"] = SimpleObject(1, 6, "world");
+    // auto
+    mMap.insert({"fourth", SimpleObject(1, 1, "number")});
+
+    
     
     return 0;
 }
