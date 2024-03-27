f9dff052474179461e5d37dbda2a117074cb1d1c
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2024-03-27 22:50:24 +0330

    Update ReadFromABinaryFile.cpp

--- ReadFromABinaryFile.cpp
+++ ReadFromABinaryFile.cpp
@@ -1,1 +1,14 @@
+#include <fstream>
+#include <iterator>
+#include <algorithm>
 
+int main()
+{
+    std::ifstream input( "C:\\Final.gif", std::ios::binary );
+    std::ofstream output( "C:\\myfile.gif", std::ios::binary );
+
+    std::copy( 
+        std::istreambuf_iterator<char>(input), 
+        std::istreambuf_iterator<char>( ),
+        std::ostreambuf_iterator<char>(output));
+}
